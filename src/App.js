b516abb7a4816d607f276e4f23f39f2ac7fee890
miaosiyu <miaosiyu@tezign.com>
2020-12-04 16:55:59 +0800

    use sass

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   NavLink,
 } from "react-router-dom";
-import Dashboard from './pages/Dashboard';
-import Test from './pages/Test';
-import styles from './App.module.less';
+import styles from './App.module.scss';
+
+const Dashboard = React.lazy(() => import('./pages/Dashboard'));
+const Assets = React.lazy(() => import('./pages/Assets'));
+const StoryBooks = React.lazy(() => import('./pages/StoryBooks'));
 
 export default function App(props) {
   const navLinks = [
     { url: '/', name: 'Dashboard' },
+    { url: '/assets', name: 'Assets' },
     { url: '/storybooks', name: 'Story books' },
   ].map(x => {
     return (
@@ -31,14 +34,19 @@ export default function App(props) {
             {navLinks}
           </div>
           <div className={styles.child}>
-            <Switch>
-              <Route exact path="/">
-                <Dashboard />
-              </Route>
-              <Route path="/storybooks">
-                <Test />
-              </Route>
-            </Switch>
+            <Suspense fallback={null}>
+              <Switch>
+                <Route exact path="/">
+                  <Dashboard />
+                </Route>
+                <Route path="/assets">
+                  <Assets />
+                </Route>
+                <Route path="/storybooks">
+                  <StoryBooks />
+                </Route>
+              </Switch>
+            </Suspense>
           </div>
         </Router>
       </div>
